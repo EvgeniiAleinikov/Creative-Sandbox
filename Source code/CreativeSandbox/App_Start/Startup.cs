8f2eb2bbe9626679224c46a1bb12247b1ee163a5
Unknown <vasilek-1997@tut.by>
2017-11-26 12:17:59 +0300

    Configure Identity

---
+++ Source code/CreativeSandbox/App_Start/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using CreativeSandbox.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(CreativeSandbox.Startup))]
+
+namespace CreativeSandbox
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // настраиваем контекст и менеджер
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
---
+++ Source code/CreativeSandbox/Models/IdentityModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CreativeSandbox.Models
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext() : base("DefaultConnection") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+    }
+}
